4dd8fe7afd39ac85b4618a55bc08c31e948cf21b
Syed Hussain Haider <syedhussainhaider5@gmail.com>
2023-05-16 19:29:12 +0500

    Added animation in create Post button

--- lib/pages/feed_page.dart
+++ lib/pages/feed_page.dart
@@ -2,10 +2,16 @@ import "dart:ffi";
 
 import "package:feedback_module/widgets/post.dart";
 import "package:flutter/material.dart";
+import "package:flutter/rendering.dart";
 
-class feed_Page extends StatelessWidget {
+class feed_Page extends StatefulWidget {
   feed_Page({super.key});
 
+  @override
+  State<feed_Page> createState() => _feed_PageState();
+}
+
+class _feed_PageState extends State<feed_Page> {
   final list = [
     [
       "Syed Hussain Haider Bukhari",
@@ -21,15 +27,65 @@ class feed_Page extends StatelessWidget {
       "Syed Hussain Haider Bukhari",
       "syedhussainhaider5@gmail.com",
       "Helllo My name is Bukharii."
+    ],
+    [
+      "Syed Hussain Haider Bukhari",
+      "syedhussainhaider5@gmail.com",
+      "Helllo My name is Bukharii."
     ]
   ];
 
+  bool _showFab = true;
+
   @override
   Widget build(BuildContext context) {
+    const duration = Duration(milliseconds: 300);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("All Posts"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.blue,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withOpacity(.60),
+        selectedFontSize: 14,
+        unselectedFontSize: 14,
+        onTap: (value) {
+          // Respond to item press.
+        },
+        items: [
+          BottomNavigationBarItem(
+            label: ('Favourite'),
+            icon: Icon(Icons.favorite),
+          ),
+          BottomNavigationBarItem(
+            label: ('Favourite'),
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: ('Favourite'),
+            icon: Icon(Icons.search),
+          ),
+          BottomNavigationBarItem(
+            label: ('Favourite'),
+            icon: Icon(Icons.light),
+          ),
+        ],
+      ),
+      floatingActionButton: AnimatedSlide(
+        duration: duration,
+        offset: _showFab ? Offset.zero : Offset(0, 2),
+        child: AnimatedOpacity(
+          duration: duration,
+          opacity: _showFab ? 1 : 0,
+          child: FloatingActionButton.extended(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+            label: Text("Create Post"),
+          ),
+        ),
       ),
       // body: ListView(
       //   children: [
@@ -59,14 +115,27 @@ class feed_Page extends StatelessWidget {
       //   ],
       // ),
 
-      body: ListView.builder(
-          itemCount: list.length,
-          itemBuilder: (context, index) {
-            return post(
-                username: list[index][0],
-                email: list[index][1],
-                text: list[index][2]);
-          }),
+      body: NotificationListener<UserScrollNotification>(
+        onNotification: (notification) {
+          final ScrollDirection direction = notification.direction;
+          setState(() {
+            if (direction == ScrollDirection.reverse) {
+              _showFab = false;
+            } else if (direction == ScrollDirection.forward) {
+              _showFab = true;
+            }
+          });
+          return true;
+        },
+        child: ListView.builder(
+            itemCount: list.length,
+            itemBuilder: (context, index) {
+              return post(
+                  username: list[index][0],
+                  email: list[index][1],
+                  text: list[index][2]);
+            }),
+      ),
       drawer: Drawer(),
     );
   }
